12e09142bcbf7ab1fe35bb6c16b86d8b6cc2ca20
Leustean Andrei <leuandrei01@gmail.com>
2018-04-02 00:09:40 +0300

    Actually fixed camera focus

---
+++ Assets/btn_action.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class btn_action : MonoBehaviour, IVirtualButtonEventHandler {
+
+    public GameObject vbBtnObj;
+    public GameObject panel;
+
+    // Use this for initialization
+    void Start () {
+        vbBtnObj = GameObject.Find("logo_button");
+        panel = GameObject.Find("Plane");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    // Update is called once per frame
+
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        panel.SetActive(true);
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        panel.SetActive(false);
+        Debug.Log("Button released");
+    }
+
+    void Update()
+    {
+
+    }
+
+}
